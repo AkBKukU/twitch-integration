26f64d632b14e851493fa28c80325f298e15d1d8
Shelby Jueden <shelby@jueden.org>
2024-08-24 09:03:06 -0700

    Remove bad chat API dump

--- stream/api_twitch.py
+++ stream/api_twitch.py
@@ -125,7 +125,6 @@ class APItwitch(APIbase):
             }
 
         self.log("callback_chat",json.dumps(message))
-        self.log("api_chat",json.dumps(chat))
         self.emit_chat(message)
         return
 
