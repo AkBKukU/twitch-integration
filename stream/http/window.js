699244fcd4d3e44e9d807b75b78e30a7cda0a454
Shelby Jueden <shelby@jueden.org>
2024-12-15 10:57:01 -0700

    Make chat names easier to read and URLs clickable on read page

--- stream/http/window.js
+++ stream/http/window.js
@@ -1,4 +1,6 @@
-
+function adjust(color, amount) {
+	    return '#' + color.replace(/^#/, '').replace(/../g, color => ('0'+Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(16)).substr(-2));
+}
 // Chat Message Handling
 function chat_read(chat_data)
 {
@@ -15,7 +17,7 @@ function chat_read(chat_data)
 
         var name = document.createElement("span");
         name.innerHTML = c['from'];
-        name.style.color = c['color'];
+        name.style.color = adjust(c['color'],30);
         name.classList.add("name");
         li.appendChild(name)
 
